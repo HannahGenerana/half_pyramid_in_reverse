2e40c1a67c8bbcf720f8299b171ca95145d736a1
HannahGenerana <hannahgenerana@gmail.com>
2024-01-20 20:24:39 +0800

    line of asterisk

--- hald_pyramid_in_reverse.py
+++ hald_pyramid_in_reverse.py
@@ -3,8 +3,14 @@
 # pseudocode
 
 # create while loop with a condition of a variable is greater than or equal to another variable
+tree = 1
+line_asterisk = 10
+
+while line_asterisk >= tree:
 
 # create a line of asterisk
+    for i in range (line_asterisk):
+        print ('*',end='')
 
 # subtract the value of the variable that create a line of asterisk from 1 to reduce the value it will print
 
