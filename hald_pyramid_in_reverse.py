a36be81ffa7ac9c5ddfc87ca76a7b2a7613eb608
HannahGenerana <hannahgenerana@gmail.com>
2024-01-20 20:19:47 +0800

    pseudocode

---
+++ hald_pyramid_in_reverse.py
@@ -0,0 +1,11 @@
+# Print a downward Half-Pyramid Pattern of Star (asterisk)
+
+# pseudocode
+
+# create while loop with a condition of a variable is greater than or equal to another variable
+
+# create a line of asterisk
+
+# subtract the value of the variable that create a line of asterisk from 1 to reduce the value it will print
+
+# make an empty print to start the next print at a new line
